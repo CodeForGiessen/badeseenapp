b38eede115d9afdb0846d28c2ba4dc1599b46839
Florian Kolb <florian.kolb@mni.thm.de>
2015-03-04 20:57:08 +0100

    add: new Favorites

--- app/scripts/services/LakeDataFavorite.js
+++ app/scripts/services/LakeDataFavorite.js
@@ -94,31 +94,115 @@ angular.module('badeseenApp').factory('LakeDataFavorite',
         }
     	},
     	{
-        "name": "Edertalsperre - Asel Süd",
-        "hlugurl": "http://badeseen.hlug.de/badegewaesser/waldeck-frankenberg/edertalsperre-asel-sued.html",
-        "latitude": "51.182400",
-        "longitude": "8.952400",
-        "openFrom": "2014-06-15T22:00:00.000Z",
-        "openTo": "2014-08-31T22:00:00.000Z",
-        "introtext": "Was für ein Stausee! Mehr als 25 Millionen Goldmark hat er das Königreich Preussen gekostet! 1914 war das gewaltige Bauwerk fertiggestellt: Stärke der Staumauer an der Sohle 36 m, Länge an der Krone 400 m, Stauhöhe 47 m, Länge des Sees 27 km mit einer Uferlänge von 69 km. Als das Wasser der Eder sich staute, mussten etwa 900 Menschen ihre Häuser verlassen. Die Errichtung eines großen Wasserreservoirs war erforderlich für die Speisung des neu gebauten Mittellandkanals. Zusätzlich schützte man so das untere Edertal und die Fulda gegen Hochwasser und konnte die Wasserkraft zur Gewinnung von Elektrizität nutzen. Der See wirkt aber auch elektrisierend auf Freunde des Wassersports und Erholungssuchende aus ganz Deutschland, in dessen Mitte dieses Ferienparadies liegt.",
+        "name": "Heuchelheimer See II",
+        "hlugurl": "http://badeseen.hlug.de/badegewaesser/giessen/heuchelheimer-see-ii.html",
+        "latitude": "50.565300",
+        "longitude": "8.625000",
+        "openFrom": "2014-05-31T22:00:00.000Z",
+        "openTo": "2014-08-30T22:00:00.000Z",
+        "introtext": "Der 8,8 ha große See hat meist sehr sauberes Wasser. Er wird wegen einer Wasserskianlage besonders von Wasserski- und Wakeboard-Fahrern geschätzt, jedoch besitzt er am südöstlichen Ufer auch eine Liegewiese von der man zum Baden ans Wasser gelangt.",
         "measurements": [
             {
-                "date": "2014-08-11T22:00:00.000Z",
-                "waterTemperature": 21,
-                "enterocsocci": "10",
-                "escherichiaColi": "130",
+                "date": "2014-08-18T22:00:00.000Z",
+                "waterTemperature": 20,
+                "enterocsocci": "15",
+                "escherichiaColi": "15",
                 "rating": 1,
                 "comment": ""
             },
             {
-                "date": "2014-07-21T22:00:00.000Z",
-                "waterTemperature": 24,
-                "enterocsocci": "21",
-                "escherichiaColi": "21",
+                "date": "2014-08-04T22:00:00.000Z",
+                "waterTemperature": 23,
+                "enterocsocci": "30",
+                "escherichiaColi": "15",
                 "rating": 1,
                 "comment": ""
-            }],
-            "yearratings": [
+            }
+        ],
+        "yearratings": [
+            {
+                "year": "2011",
+                "rating": 1
+            },
+            {
+                "year": "2012",
+                "rating": 1
+            },
+            {
+                "year": "2013",
+                "rating": 1
+            }
+        ],
+        "images": [
+            {
+                "src": "http://badeseen.hlug.de/fileadmin/img_content/badeseen/Giessen/Heuchelheimer_See_II/Heuchelheimer_See_2_03.JPG",
+                "copyright": "http://badeseen.hlug.de© HLUG"
+            },
+            {
+                "src": "http://badeseen.hlug.de/fileadmin/img_content/badeseen/Giessen/Heuchelheimer_See_II/Heuchelheimer_See_2_02.JPG",
+                "copyright": "http://badeseen.hlug.de© HLUG"
+            },
+            {
+                "src": "http://badeseen.hlug.de/fileadmin/img_content/badeseen/Giessen/Heuchelheimer_See_II/Heuchelheimer_See_2_01_T.JPG",
+                "copyright": "http://badeseen.hlug.de© HLUG"
+            }
+        ],
+        "city": "Heuchelheim",
+        "heightAboveSeaLevel": 160,
+        "areaHa": 8.8,
+        "depthMax": 11.58,
+        "depthAvg": 5.71,
+        "lakeType": "Baggersee",
+        "extracurricularActivity": [
+            "Baden",
+            "Wasserski"
+        ],
+        "blueGreenAlgeaRisk": "gering",
+        "operator": {
+            "name": "Dr. Uwe Vorpahl",
+            "street": "Beethovenstraße 18",
+            "zipcodeCity": "35452 Heuchelheim",
+            "email": "",
+            "telephone": "0641/6868888",
+            "fax": "",
+            "website": ""
+        },
+        "appropriateAuthority": {
+            "name": "Gesundheitsamt Landkreis Gießen",
+            "address": "Landkreis Gießen",
+            "addressAdditional": "Kreisgesundheitsamt",
+            "street": "Riversplatz 1-9",
+            "zipcodeCity": "35394 Gießen",
+            "telephone": "0641 9390-1422"
+        }
+    },
+    {
+        "name": "Dutenhofener Badesee",
+        "hlugurl": "http://badeseen.hlug.de/badegewaesser/lahn-dill-kreis/dutenhofener-badesee.html",
+        "latitude": "50.565700",
+        "longitude": "8.612100",
+        "openFrom": "2014-05-18T22:00:00.000Z",
+        "openTo": "2014-08-31T22:00:00.000Z",
+        "introtext": "Im Tal der Lahn zwischen Gießen und Wetzlar ist der Dutenhofer See mit 29,4 ha Fläche nicht der einzige Badesee, der durch das Ausbaggern von Kiesvorkommen entstanden ist. Die Umgebung ist als Erholungs- und Naturschutzgebiet beliebtes Ziel für einen sommerlichen Badeausflug. Das ansonsten klare Wasser kann durch Algenwachstum grünlich werden, ein Sandstrand wird jährlich für die Badesaison neu aufgeschüttet.",
+        "measurements": [
+            {
+                "date": "2014-08-17T22:00:00.000Z",
+                "waterTemperature": 19,
+                "enterocsocci": "15",
+                "escherichiaColi": "30",
+                "rating": 1,
+                "comment": ""
+            },
+            {
+                "date": "2014-07-20T22:00:00.000Z",
+                "waterTemperature": 25,
+                "enterocsocci": "<15",
+                "escherichiaColi": "<15",
+                "rating": 1,
+                "comment": ""
+            }
+        ],
+        "yearratings": [
             {
                 "year": "2011",
                 "rating": 1
@@ -132,36 +216,48 @@ angular.module('badeseenApp').factory('LakeDataFavorite',
                 "rating": 2
             }
         ],
-        "images": [],
-        "city": "Vöhl",
-        "heightAboveSeaLevel": 245,
-        "areaHa": 1.05,
-        "depthMax": 15,
-        "depthAvg": 7,
-        "lakeType": "Talsprerre",
+        "images": [
+            {
+                "src": "http://badeseen.hlug.de/fileadmin/img_content/badeseen/Lahn-Dill-Kreis/Dutenhofener_Badesee/Dutenhofener_See03.JPG",
+                "copyright": "http://badeseen.hlug.de© HLUG"
+            },
+            {
+                "src": "http://badeseen.hlug.de/fileadmin/img_content/badeseen/Lahn-Dill-Kreis/Dutenhofener_Badesee/Dutenhofener_See02.JPG",
+                "copyright": "http://badeseen.hlug.de© HLUG"
+            },
+            {
+                "src": "http://badeseen.hlug.de/fileadmin/img_content/badeseen/Lahn-Dill-Kreis/Dutenhofener_Badesee/Dutenhofener_See01_T.JPG",
+                "copyright": "http://badeseen.hlug.de© HLUG"
+            }
+        ],
+        "city": "Wetzlar",
+        "heightAboveSeaLevel": 133,
+        "areaHa": 29.4,
+        "depthMax": 11.4,
+        "depthAvg": 4.04,
+        "lakeType": "Baggersee",
         "extracurricularActivity": [
             "Angelsport",
             "Baden",
-            "Sporttauchen",
-            "Wassersport"
+            "Segeln"
         ],
         "blueGreenAlgeaRisk": "hoch",
         "operator": {
-            "name": "Gemeinde Vöhl",
-            "street": "Schloßstr.1",
-            "zipcodeCity": "34516 Vöhl",
-            "email": "bauamt@voehl.de",
-            "telephone": "05635-9931-11",
-            "fax": "05635-9931-99",
-            "website": "www.voehl.de"
+            "name": "Strasser, Müller, Auffeger GbR",
+            "street": "Am Dutenhofener See 1",
+            "zipcodeCity": "35582 Wetzlar-Dutenhofen",
+            "email": "info@dutenhofenersee.de",
+            "telephone": "0641 21245",
+            "fax": "0641 250 95 25",
+            "website": "www.dutenhofenersee.de"
         },
         "appropriateAuthority": {
-            "name": "Gesundheitsamt Waldeck-Frankenberg",
-            "address": "Kreis Waldeck-Frankenberg",
-            "addressAdditional": "Kreisgesundheitsamt",
-            "street": "Am Kniep 50",
-            "zipcodeCity": "34497 Korbach",
-            "telephone": "05631 954-0"
+            "name": "Gesundheitsamt Lahn-Dill-Kreis",
+            "address": "Lahn-Dill-Kreis",
+            "addressAdditional": "-Abt. Gesundheit-",
+            "street": "Schlossstr. 20",
+            "zipcodeCity": "35745 Herborn",
+            "telephone": "06441 407-1616"
         }
     }
 		]
