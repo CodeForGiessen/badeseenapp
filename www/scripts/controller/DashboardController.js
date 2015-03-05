9abaaf43881b80ef4c2288cc48f8455953e40c36
Vincent Elliott Wagner <Vincent.Elliott.Wagner@mni.thm.de>
2015-03-05 12:55:32 +0100

    removed www folder from repo

--- www/scripts/controller/DashboardController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-angular.module('badeseenApp').controller('DashboardController',
-	function ($scope, LakeDataFavorite, FavData) {
-		LakeDataFavorite.getData();
-    	$scope.favorites = FavData.getAll();  
-	});
