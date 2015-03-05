0231ac68cd156edb7725620c817bc0bd40271961
Florian Kolb <florian.kolb@mni.thm.de>
2015-03-05 16:03:16 +0100

    update: dashboard

--- app/scripts/controller/DashboardController.js
+++ app/scripts/controller/DashboardController.js
@@ -1,6 +1,19 @@
 "use strict";
 angular.module('badeseenApp').controller('DashboardController',
-	function ($scope, LakeDataFavorite, FavData) {
+	function ($scope, LakeDataFavorite, FavData, $ionicModal) {
 		LakeDataFavorite.getData();
     	$scope.favorites = FavData.getAll();  
+
+    	$ionicModal.fromTemplateUrl('templates/lakeModal.html', {
+			scope: $scope,
+			animation: 'slide-in-up'
+		})
+		.then(function (modal) {
+			$scope.modal = modal;
+		});
+
+		$scope.openModal = function (lake) {
+			$scope.modal.show();
+			$scope.lake = lake;
+		};
 	});
