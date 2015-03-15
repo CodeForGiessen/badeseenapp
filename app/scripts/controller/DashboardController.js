98e04a0b2e3e4a9d14df8fa3bf500db2bfd32c20
Marco Schaefer <gogomsfr@gmail.com>
2015-03-15 23:52:10 +0100

    Add: Favorit Card

--- app/scripts/controller/DashboardController.js
+++ app/scripts/controller/DashboardController.js
@@ -1,27 +1,42 @@
 'use strict';
 angular.module('badeseenApp').controller('DashboardController',
-	function ($scope, FavData, LakeData, $q, $state) {
+	function ($scope, FavData, LakeData, $q, $state, WeatherData) {
     	$scope.favorites = [];
-         
+
         $scope.goToLakeList = function () {
             $state.go ('tabs.lakeList');
         };
 
         $scope.removeFavorite = function (lakeID) {
+            FavData.remove(lakeID);
+            $state.go($state.current, {}, {reload: true});
+        };
 
+        $scope.goToLake = function(lakeID) {
+            $state.go('lake',{ id: lakeID });
         };
 
         $scope.$on('$ionicView.enter', function(){
             console.log('enter');
             var lakeIds = FavData.getAll();
-            $q.all(lakeIds.map(function(id){
-                return LakeData.getById(id);
-            }))
-            .then(function(lakes){
-                $scope.favorites = lakes;
+            $q.all([
+                $q.all(
+                lakeIds.map(function(id){
+                    return LakeData.getById(id);
+                })),
+                WeatherData.getAll(),
+
+            ])
+            .then(function(res){
+                var favoriteLakes = res[0];
+                var weatherDataAllLakes = res[1];
+
+                $scope.favorites = favoriteLakes;
+                $scope.weatherData = weatherDataAllLakes;
             })
             .catch(function(){
                 //TODO handle
             });
         });
+
 	});
