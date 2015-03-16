eb2de64bcbf3be7ee10fa44ac001bc535b63df6c
Vincent Elliott Wagner <Vincent.Elliott.Wagner@mni.thm.de>
2015-03-16 11:36:15 +0100

    added loading and errorhandling to dashboard view

--- app/scripts/controller/DashboardController.js
+++ app/scripts/controller/DashboardController.js
@@ -1,7 +1,11 @@
 'use strict';
 angular.module('badeseenApp').controller('DashboardController',
-	function ($scope, FavData, LakeData, $q, $state, WeatherData) {
+	function ($scope, FavData, LakeData, $q, $state, WeatherData, $ionicLoading, $timeout) {
     	$scope.favorites = [];
+
+        $scope.error = false;
+        $scope.init = true;
+        var first = false;
 
         var chunk = function(arr, size) {
             var newArr = [];
@@ -9,7 +13,48 @@ angular.module('badeseenApp').controller('DashboardController',
                 newArr.push(arr.slice(i, i+size));
             }
             return newArr;
-        }
+        };
+
+         var reload = function(){
+            $ionicLoading.show();
+            
+            var lakeIds = FavData.getAll();
+            $q.all([
+                $q.all(
+                lakeIds.map(function(id){
+                    return LakeData.getById(id);
+                })),
+                WeatherData.getAll(),
+            ])
+            .then(function(res){
+                var favoriteLakes = res[0];
+                var weatherDataAllLakes = res[1];
+
+                $scope.rows = chunk(favoriteLakes,2);
+                $scope.favorites = favoriteLakes;
+                $scope.weatherData = weatherDataAllLakes;
+                $scope.error = false;
+                $scope.init = false;   
+            })
+            .catch(function(err){ 
+                console.log(err);
+                $scope.error = true;
+            })
+            .finally(function(){
+                $ionicLoading.hide();
+                $timeout(function(){
+                    ionic.trigger('resize',{
+                        target:'window'
+                    });
+                });
+            });
+        };
+
+        $scope.reload = reload;
+
+        $scope.$on('$ionicView.enter', reload);
+
+
 
         $scope.goToLakeList = function () {
             $state.go ('tabs.lakeList');
@@ -23,32 +68,4 @@ angular.module('badeseenApp').controller('DashboardController',
         $scope.goToLake = function(lakeID) {
             $state.go('lake',{ id: lakeID });
         };
-
-        $scope.$on('$ionicView.enter', function(){
-            console.log('enter');
-            var lakeIds = FavData.getAll();
-            $q.all([
-                $q.all(
-                lakeIds.map(function(id){
-                    return LakeData.getById(id);
-                })),
-                WeatherData.getAll(),
-
-            ])
-            .then(function(res){
-                var favoriteLakes = res[0];
-                var weatherDataAllLakes = res[1];
-
-                $scope.rows = chunk(favoriteLakes,2);
-                $scope.favorites = favoriteLakes;
-                $scope.weatherData = weatherDataAllLakes;
-            })
-            .catch(function(){
-                //TODO handle
-            });
-        });
-
-        
-        
-
 	});
